d56cda61f8b44f4728b43a150b06f4e25dc2a0cf
JennyBortnouskaya <gienie.b17@gmail.com>
2019-03-24 23:23:10 +0300

    added: lexical changes

--- src/index.js
+++ src/index.js
@@ -1,43 +1,36 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-let coins = currency;	
-let h = 50; 
-let q = 25; 
-let d = 10; 
-let n = 5; 
-let p = 1;
-let makeExchange = {};
+    let money = {};
     
-if(coins <= 10000) { 
-    let result_H = (coins - coins % h)/h; 
+    if (currency >= 10000) {
+        return {
+            error: "You are rich, my friend! We don't have so much coins for exchange"
+        }
+    }
 
-    if(result_H > 0){
-    makeExchange.H = result_H;
-    };
-    let result_Q = (coins % h - coins % h % q)/q;
+    if (currency >= 50) {
+        money.H = Math.floor(currency / 50);
+        currency %= 50;
+    }
 
-    if(result_Q > 0){
-     makeExchange.Q = result_Q;   
-    };
-    let result_D = (coins % h % q - coins % h % q % d)/d; 
+    if (currency >= 25) {
+        money.Q = Math.floor(currency / 25);
+        currency %= 25;
+    }
 
-    if(result_D > 0){  
-    makeExchange.D = result_D;
-    };
-    let result_N = (coins % h % q % d - coins % h % q % d % n)/n; 
+    if (currency >= 10) {
+        money.D = Math.floor(currency / 10);
+        currency %= 10;
+    }
+
+    if (currency >= 5) {
+        money.N = Math.floor(currency / 5);
+        currency %= 5;
+    }
+
+    if (currency >= 1) {
+        money.P = Math.floor(currency / 1);
+        currency %= 1;
+    }
     
-    if(result_N > 0){
-    makeExchange.N = result_N;
-    };    
-    let result_P = (coins % h % q % d % n -  coins % h % q % d % n % p)/p; 
-
-    if(result_P > 0){
-    makeExchange.P = result_P;
-    }; 
-
-    } else { 
-        makeExchange.error = "You are rich, my friend! We don't have so much coins for exchange"};
-        return makeExchange;
+    return money;
 }
